3818cdbac26a7a726e1b097892e5e089e3767e69
Eberhard Hansis <git@ehansis.de>
2019-03-01 09:13:23 +0100

    fizzbuzz as typing exercise

--- kata20190226/fizzbuzz.py
+++ kata20190226/fizzbuzz.py
@@ -28,3 +28,51 @@ def ff(n):
             out.append(str(i))
 
     return out
+
+
+def fff(n):
+    out = []
+    for i in range(1, n + 1):
+        if i % 3 == 0:
+            if i % 5 == 0:
+                out.append("fizzbuzz")
+            else:
+                out.append("fizz")
+        elif i % 5 == 0:
+            out.append("buzz")
+        else:
+            out.append(str(i))
+
+    return out
+
+def ffff(n):
+    out = []
+    for i in range(1, n + 1):
+        if i % 3 == 0:
+            if i % 5 == 0:
+                out.append("fizzbuzz")
+            else:
+                out.append("fizz")
+        elif i % 5 == 0:
+            out.append("buzz")
+        else:
+            out.append(str(i))
+
+    return out
+
+
+def fffff(n):
+    out = []
+    for i in range(1, n + 1):
+        if i % 3 == 0:
+            if i % 5 == 0:
+                out.append("fizzbuzz")
+            else:
+                out.append("fizz")
+        elif i % 5 == 0:
+            out.append("buzz")
+        else:
+            out.append(str(i))
+
+    return out
+
