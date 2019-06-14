9f5ae578bfc560d548ee2fd474c47b8e8053b5c4
Eberhard Hansis <git@ehansis.de>
2019-06-14 09:44:42 +0200

    Neo4j experiments

--- kata20190613/neo4j_noob.py
+++ kata20190613/neo4j_noob.py
@@ -3,28 +3,87 @@ import os
 from neo4j import GraphDatabase
 
 
-TEST_LABELS = {
-    'Node': 'TestNode',
-}
+TEST_LABELS = ['TestNode']
+
+
+_driver = None
 
 
 def get_driver():
-    uri = "bolt://localhost:7687"
-    driver = GraphDatabase.driver(uri, auth=("neo4j", os.environ['NEO4J_PASSWORD']))
-    return driver
+    global _driver
+
+    if _driver is None:
+        uri = "bolt://localhost:7687"
+        driver = GraphDatabase.driver(uri, auth=("neo4j", os.environ['NEO4J_PASSWORD']))
+        _driver = driver
+
+    return _driver
 
 
 def create_node(name):
     driver = get_driver()
     with driver.session() as session:
-        return session.run("CREATE (a:%s {name:$name}) "
-                           "RETURN id(a)" % TEST_LABELS['Node'],
+        return session.run("CREATE (a:TestNode {name:$name}) "
+                           "RETURN id(a)",
                            name=name).single().value()
+
+
+def create_relationship(label, name_from, name_to):
+    driver = get_driver()
+    with driver.session() as session:
+        # using parameters for relationship types is not supported, see
+        # https://neo4j.com/docs/cypher-manual/current/syntax/parameters/
+        return session.run("MATCH (from:TestNode {name:$name_from}),"
+                           "      (to:TestNode {name:$name_to})"
+                           "CREATE (from)-[rel:%(label)s]->(to)"
+                           "RETURN id(rel)" % {'label': label},
+                           name_from=name_from, name_to=name_to)
+
+
+def query_relationship_count():
+    driver = get_driver()
+    with driver.session() as session:
+        res = session.run("MATCH ()-[rel]->()"
+                          "RETURN COUNT(rel)").single()
+        if res is None:
+            return 0
+        else:
+            return res.value()
+
+
+def query_node_count():
+    driver = get_driver()
+    with driver.session() as session:
+        res = session.run("MATCH (n)"
+                          "RETURN COUNT(n)").single()
+        if res is None:
+            return 0
+        else:
+            return res.value()
+
+
+def query_relationships():
+    driver = get_driver()
+    with driver.session() as session:
+        return session.run("MATCH ()-[rel]->()"
+                           "RETURN rel")
+
+
+def create_relationship_with_merge(label, name_from, name_to):
+    driver = get_driver()
+    with driver.session() as session:
+        # using parameters for relationship types is not supported, see
+        # https://neo4j.com/docs/cypher-manual/current/syntax/parameters/
+        return session.run("MERGE (from:TestNode {name:$name_from})"
+                           "MERGE (to:TestNode {name:$name_to})"
+                           "MERGE (from)-[rel:%(label)s]->(to)"
+                           "RETURN ID(rel)" % {'label': label},
+                           name_from=name_from, name_to=name_to)
 
 
 def cleanup():
     driver = get_driver()
     with driver.session() as session:
-        for label in TEST_LABELS.values():
+        for label in TEST_LABELS:
             session.run("MATCH (n:%s)"
                         "DETACH DELETE n" % label)
--- kata20190613/test_neo4j.py
+++ kata20190613/test_neo4j.py
@@ -6,6 +6,48 @@ def test_get_driver():
 
 
 def test_create_node():
+    neo4j_noob.cleanup()
     i = neo4j_noob.create_node('FooBar')
     assert i >= 0
+
+
+def test_create_relationship():
     neo4j_noob.cleanup()
+    neo4j_noob.create_node('Node1')
+    neo4j_noob.create_node('Node2')
+    assert neo4j_noob.query_node_count() == 2
+
+    res = neo4j_noob.create_relationship('likes', 'Node1', 'Node2')
+    assert res.single().value() >= 0
+
+    count = neo4j_noob.query_relationship_count()
+    assert count == 1
+
+    rel = neo4j_noob.query_relationships().records()
+    count_2 = 0
+    for _ in rel:
+        count_2 += 1
+    assert count_2 == 1
+
+
+def test_create_relationship_node_missing():
+    neo4j_noob.cleanup()
+    neo4j_noob.create_node('Node1')
+    res = neo4j_noob.create_relationship('likes', 'Node1', 'Node2')
+    assert res.single() is None
+    assert neo4j_noob.query_relationship_count() == 0
+
+
+def test_create_relationship_with_merge():
+    neo4j_noob.cleanup()
+    neo4j_noob.create_relationship_with_merge('likes', 'Node1', 'Node2')
+    assert neo4j_noob.query_relationship_count() == 1
+    assert neo4j_noob.query_node_count() == 2
+
+    neo4j_noob.create_relationship_with_merge('hates', 'Node2', 'Node3')
+    assert neo4j_noob.query_relationship_count() == 2
+    assert neo4j_noob.query_node_count() == 3
+
+    neo4j_noob.create_relationship_with_merge('loves', 'Node2', 'Node1')
+    assert neo4j_noob.query_relationship_count() == 3
+    assert neo4j_noob.query_node_count() == 3
